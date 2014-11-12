e3a9982df33ff0d374dcd97651feb8fb552d4453
rsperanza <rsperanza@blackberry.com>
2014-11-12 09:36:01 -0500

    Sync up Hub integration samples from working set.

--- HeadlessHubIntegration/src/main.cpp
+++ HeadlessHubIntegration/src/main.cpp
@@ -5,9 +5,43 @@
 
 using namespace bb;
 
+void myMessageOutput(QtMsgType type, const char* msg) {
+    // setup shared logs folder
+    QString logFilePath = (new QDir())->absoluteFilePath("shared/documents/logs");
+    QDir sharedDir(logFilePath);
+
+    if (!sharedDir.exists()) {
+    sharedDir.mkpath (logFilePath);
+    }
+
+    QFileInfo fileInfo((new QDir())->absoluteFilePath(logFilePath + "/HeadlessHubIntegration.txt"));
+    QFile file((new QDir())->absoluteFilePath(logFilePath + "/HeadlessHubIntegration.txt"));
+
+    // Open the file that was created
+    if (!file.exists()) {
+        QDir().mkpath (fileInfo.dir().path());
+        // Open the file that was created
+        if (file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+            file.write(msg);
+            file.write("\n");
+            file.flush();
+            file.close();
+        }
+    } else {
+        if (file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+            file.write(msg);
+            file.write("\n");
+            file.flush();
+            file.close();
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
-	 Application app(argc, argv);
+    qInstallMsgHandler(myMessageOutput);
+
+	Application app(argc, argv);
 
 	qDebug() << "HeadlessHubIntegration: started";
 
--- HubIntegration/src/main.cpp
+++ HubIntegration/src/main.cpp
@@ -10,8 +10,42 @@
 using namespace bb::cascades;
 using namespace bb::system;
 
+void myMessageOutput(QtMsgType type, const char* msg) {
+    // setup shared logs folder
+    QString logFilePath = (new QDir())->absoluteFilePath("shared/documents/logs");
+    QDir sharedDir(logFilePath);
+
+    if (!sharedDir.exists()) {
+    sharedDir.mkpath (logFilePath);
+    }
+
+    QFileInfo fileInfo((new QDir())->absoluteFilePath(logFilePath + "/HubIntegration.txt"));
+    QFile file((new QDir())->absoluteFilePath(logFilePath + "/HubIntegration.txt"));
+
+    // Open the file that was created
+    if (!file.exists()) {
+        QDir().mkpath (fileInfo.dir().path());
+        // Open the file that was created
+        if (file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+            file.write(msg);
+            file.write("\n");
+            file.flush();
+            file.close();
+        }
+    } else {
+        if (file.open(QIODevice::WriteOnly | QIODevice::Append)) {
+            file.write(msg);
+            file.write("\n");
+            file.flush();
+            file.close();
+        }
+    }
+}
+
 Q_DECL_EXPORT int main(int argc, char **argv)
 {
+    qInstallMsgHandler(myMessageOutput);
+
     Application app(argc, argv);
 
     // Create the Application UI object, this is where the main.qml file
